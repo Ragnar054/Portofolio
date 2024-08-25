401c6f7402eeadb713ba8a8893a80110d9287c7e
Ragnar054 <nowzyx4545@gmail.com>
2024-08-25 19:36:08 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ export const metadata: Metadata = {
   themeColor: '#8B5CF6',
   robots: 'index, follow',
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
-  icons: 'https://cdn.discordapp.com/attachments/1154444966429462578/1251217816984486029/standard_1.gif?ex=666dc6e6&is=666c7566&hm=3ef26eab048629c1a3c213fe3c36b74223d96ea962338b65d3d5d4693b59350d&',
+  icons: 'https://cdn.discordapp.com/attachments/1174845295209742386/1276651201320190037/8838b17ac239227c26d7c96520898020.png?ex=66cc47d5&is=66caf655&hm=6c51ed00268b479cbd471c9c86bd0ed4895af83dfa79a49b018c14d9c1a8a99f&',
   openGraph: {
-    images: 'https://media.discordapp.net/attachments/1174845295209742386/1276651201320190037/8838b17ac239227c26d7c96520898020.png?ex=66cc47d5&is=66caf655&hm=6c51ed00268b479cbd471c9c86bd0ed4895af83dfa79a49b018c14d9c1a8a99f&=&format=webp&quality=lossless&width=212&height=367',
+    images: 'https://cdn.discordapp.com/attachments/1174845295209742386/1276651201320190037/8838b17ac239227c26d7c96520898020.png?ex=66cc47d5&is=66caf655&hm=6c51ed00268b479cbd471c9c86bd0ed4895af83dfa79a49b018c14d9c1a8a99f&',
   },
 };
 
